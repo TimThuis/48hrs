8119d4f290d18004a73e23ff00297b73839656b8
TimThuis <tim.thuis@gmail.com>
2016-03-16 12:14:42 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+Booking.destroy_all
+Tour.destroy_all
+Review.destroy_all
+
+puts "create user"
+
+quoc = User.create!(email: "q@email.com", password: "12345678", guide: true, first_name: "Quoc", last_name: "Bui", birthday: "1990-11-02", picture_url: "https://unsplash.it/200/200/", street: "Vondelparklaan 22", zipcode: "1234 AB", city: "Amsterdam", phone:"+31 6 1234 5678", bio: "This is me", hour_rate: 10)
+tim = User.create!(email: "tim@email.com", password: "12345678", guide: false, first_name: "Tim", last_name: "Thuis", phone:"+31 6 1234 5678")
+
+puts "create booking"
+
+# first_booking = Booking.create!(user:, tour:, date:, check_in:, check_out:, status:, total_price:, hour_rate:)
+puts "create tour"
+
+# first_tour = Tour.create!(user: tim, category: "sport")
+puts "create review"
+
+# first_review = Review.create!(content: "Hello this is my first review", rating: 5)
+
+puts "users, booking, tour, review created!"
+
+
