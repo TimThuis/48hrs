71b1ec4c4805b289998cb2cbde796cfa5e8e8838
Quoc Bui <bui.vquoc@gmail.com>
2016-03-25 13:22:37 +0100

    style  guide & tours pages

---
+++ app/assets/javascripts/modal_profile.js
@@ -0,0 +1,6 @@
+$(document).on('click', '.guide-card-container', function(e) {
+  e.stopPropagation();
+})
+$(document).on('click', '.modal-profile', function() {
+  $('.modal-profile').removeClass('is-active')
+})
