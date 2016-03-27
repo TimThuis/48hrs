f60aa3a2ff1b24094f70cec835af34dd69c4c3cf
TimThuis <tim.thuis@gmail.com>
2016-03-27 12:43:52 +0200

    add new fiels

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,27 +14,33 @@ Category.destroy_all
 
 puts "create user"
 # Guides
-quoc = User.create!(email: "q@email.com", password: "12345678", guide: true, first_name: "Quoc", last_name: "Bui", birthday: "1990-11-02", photo: "", street: "Vondelparklaan 22", zipcode: "1234 AB", city: "Amsterdam", phone:"+31 6 1234 5678", bio: "This is Q", hour_rate: 10, rating: 3)
-blane = User.create!(email: "blane@email.com", password: "12345678", guide: true, first_name: "Blane", last_name: "Bmatsardji", birthday: "1992-05-21", photo: "", street: "De pijp 100", zipcode: "1234 AB", city: "Amsterdam", phone:"+31 6 1234 5678", bio: "This is Blane", hour_rate: 15, rating: 4)
+quoc = User.create!(email: "q@email.com", password: "12345678", guide: true, first_name: "Quoc", last_name: "Bui", \
+birthday: "1990-11-02", photo: "", street: "Vondelparklaan 22", zipcode: "1234 AB", city: "Amsterdam", \
+phone:"+31 6 1234 5678", bio: "This is Q", hour_rate: 10, rating: 3, photo: "")
+
+blane = User.create!(email: "blane@email.com", password: "12345678", guide: true, first_name: "Blane", last_name: "Matsardji", \
+birthday: "1992-05-21", photo: "", street: "De pijp 100", zipcode: "1234 AB", city: "Amsterdam", \
+phone:"+31 6 1234 5678", bio: "This is Blane", hour_rate: 15, rating: 4, photo: "")
+
 # Visitor
-tim = User.create!(email: "tim@email.com", password: "12345678", guide: false, first_name: "Tim", last_name: "Thuis", phone:"+31 6 1234 5678")
-andre = User.create!(email: "andre@email.com", password: "12345678", guide: false, first_name: "Andre", last_name: "Kaas", phone:"+31 6 1234 5678")
+tim = User.create!(email: "tim@email.com", password: "12345678", guide: false, first_name: "Tim", last_name: "Thuis", phone:"+31 6 1234 5678", photo: "")
 
 ####
 
 puts "create category"
 
-sport = Category.create!(name: "Sport", description: "Action")
-party = Category.create!(name: "Party", description: "Dancing")
-food = Category.create!(name: "Food", description: "Eating")
-bar = Category.create!(name: "Bar", description: "Drinking with friends")
-cultural = Category.create!(name: "Cultural", description: "Do some sightseeing")
-shopping = Category.create!(name: "Shopping", description: "Do some sightseeing")
+active = Category.create!(name: "Active", description: "Action", photo:"")
+party = Category.create!(name: "Party", description: "Dancing", photo:"")
+food = Category.create!(name: "Food", description: "Eating", photo:"")
+bar = Category.create!(name: "Bar", description: "Drinking with friends", photo:"")
+cultural = Category.create!(name: "Cultural", description: "Do some sightseeing", photo:"")
+shopping = Category.create!(name: "Shopping", description: "Do some sightseeing", photo:"")
 
 
 ####
 
 puts "create tour"
+
 first_tour = Tour.create!(user: quoc, category: sport, description: "Sport!")
 second_tour = Tour.create!(user: quoc, category: party, description: "Party!")
 thirth_tour = Tour.create!(user: blane, category: food, description: "Food!")
@@ -43,6 +49,7 @@ fourth_tour = Tour.create!(user: blane, category: cultural, description: "Cultur
 ####
 
 puts "create booking"
+
 first_booking = Booking.create!(visitor: tim, guide: quoc, tour: first_tour, date:"2016-1-1", check_in:"", check_out:"", status:"pending", total_price: 0, hour_rate: 0)
 second_booking = Booking.create!(visitor: tim, guide: quoc, tour: second_tour, date:"2016-1-1", check_in:"", check_out:"", status:"cancel", total_price: 0, hour_rate: 0)
 thirth_booking = Booking.create!(visitor: andre, guide: quoc, tour: thirth_tour, date:"2016-1-1", check_in:"", check_out:"", status:"confirm", total_price: 0, hour_rate: 0)
@@ -50,6 +57,7 @@ fourth_booking = Booking.create!(visitor: andre, guide: quoc, tour: fourth_tour,
 ####
 
 puts "create review"
+
 first_review = Review.create!(booking: first_booking ,content: "First review of Q", rating: 5)
 second_review = Review.create!(booking: second_booking ,content: "Second review of Q", rating: 5)
 thirth_review = Review.create!(booking: thirth_booking ,content: "First review of Blane", rating: 5)
