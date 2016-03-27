0b2e702fae55168ada65a9491a96fb21d9346860
TimThuis <tim.thuis@gmail.com>
2016-03-27 12:37:09 +0200

    add field to category model'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324150816) do
+ActiveRecord::Schema.define(version: 20160327103048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20160324150816) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "photo"
   end
 
   create_table "reviews", force: :cascade do |t|
