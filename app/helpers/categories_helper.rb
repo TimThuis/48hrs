3656366679dbfe51e102697027b12d5c949bcba5
Quoc Bui <bui.vquoc@gmail.com>
2016-03-30 14:30:05 +0200

    add dynamic icon category

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,4 +1,33 @@
 module CategoriesHelper
+  def category_icon_css_class(category)
+    return "mdi" unless category.is_a?(Category)
+
+    name = category.name.underscore
+    case name
+    when "food"
+      "mdi mdi-food-variant"
+    # TODO: when cultural, etc.
+    when "active"
+     "mdi mdi-bike"
+    when "cultural"
+     "mdi mdi-church"
+    when "bar"
+     "mdi mdi-beer"
+    when "shopping"
+     "mdi mdi-shopping"
+    when "party"
+     "mdi mdi-weather-night"
+    end
+  end
+
+  def category_item_css_class(category)
+    if category.is_a?(Category)
+      "category-item category-item-#{category.name.underscore}"
+    else
+      "category-item"
+    end
+  end
+
   def category_banner_css_class(category)
     if category.is_a?(Category)
       "category-banner category-banner-#{category.name.underscore}"
